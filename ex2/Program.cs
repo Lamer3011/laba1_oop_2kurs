c3d030bf87c64aaa6e64c5752bd0bf2d87e75f3a
Kireev <Kireev@DESKTOP-PF4DRIV>
2021-09-17 13:35:13 +0300

    Add project files.

---
+++ ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time();
+
+            double x1, x2, x3, y1,
+                condition;
+            //X1
+            Console.Write("Enter x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            //X2
+            Console.Write("Enter x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            //X3
+            Console.Write("Enter x3: ");
+            x3 = Convert.ToDouble(Console.ReadLine());
+            //Y1
+            Console.Write("Enter y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            y1 = y1 + 7 / 3;
+            //Task:
+            condition = 6.3 * Math.Sin(1.3 * x1 - 3) - x2 + Math.Sqrt(x3 + 9/4) + Math.Pow(y1, 2/3);
+
+            Console.WriteLine($"Condition = 6.3 * sin(1.3 * {x1} - 3) - {x2} + √({x3} + 9/4) + ({y1} + 7/3)^2/3 = {condition}");
+        }
+
+        static void Time()
+        {
+            Console.WriteLine($"Current time: {System.DateTime.Now}\n");
+        }
+    }
+}
---
+++ laba1_oop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace laba1_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatingSystem OS = System.Environment.OSVersion;
+            Console.WriteLine($"Your OS {OS.Platform} \nCurrent time: {System.DateTime.Now}");
+        }
+    }
+}
